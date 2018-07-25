251ba7a17c5385d0f90383b9dcaf419f06f4b7c0
lxhfight <lxhfight51@outlook.com>
2018-07-25 11:23:43 +0800

    init project via vue-cli

---
+++ src/tools/storage.js
@@ -0,0 +1,64 @@
+/**
+ * Created by lxhfight on 2018/7/25.
+ * Email: lxhfight1@gmail.com
+ * Description:
+ *  该工具包含前端缓存相关方法
+ */
+
+export const SessionStorage = {
+  save (key, item) {
+    if (!window.sessionStorage) {
+      console.warn('當前瀏覽器不支持sessionStorage')
+      return null
+    }
+    if (typeof item === 'object') {
+      window.sessionStorage.setItem(key, JSON.stringify(item))
+    } else {
+      window.sessionStorage.setItem(key, item)
+    }
+  },
+  get (key, isObject = false) {
+    if (!window.sessionStorage) {
+      console.warn('當前瀏覽器不支持sessionStorage')
+      return null
+    }
+    const str = window.sessionStorage.getItem(key)
+    return str ? (isObject ? JSON.parse(str) : str) : null
+  },
+  remove (key) {
+    if (!window.sessionStorage) {
+      console.warn('當前瀏覽器不支持localStorage')
+      return null
+    }
+    window.sessionStorage.removeItem(key)
+  }
+}
+
+export const LocalStorage = {
+  save (key, item) {
+    if (!window.localStorage) {
+      console.warn('當前瀏覽器不支持localStorage')
+      return null
+    }
+    if (typeof item === 'object') {
+      window.localStorage.setItem(key, JSON.stringify(item))
+    } else {
+      window.localStorage.setItem(key, item)
+    }
+  },
+  get (key, isObject = false) {
+    if (!window.localStorage) {
+      console.warn('當前瀏覽器不支持localStorage')
+      return null
+    }
+    const str = window.localStorage.getItem(key)
+    return str ? (isObject ? JSON.parse(str) : str) : null
+  },
+  remove (key) {
+    if (!window.localStorage) {
+      console.warn('當前瀏覽器不支持localStorage')
+      return null
+    }
+    window.localStorage.removeItem(key)
+  }
+}
