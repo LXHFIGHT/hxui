50a0a9fc5ff3ff00656d09d25fa230136cde4dc9
lxhfight <lxhfight51@outlook.com>
2018-08-07 21:48:37 +0800

    添加头部组件: hx-header

---
+++ src/hxui/cpts/HxHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hx-header">
+    <span class="title" v-text="title"></span>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String
+    }
+  }
+}
+</script>
