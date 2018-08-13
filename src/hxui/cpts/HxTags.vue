37aa882d2bfd79da6192fe2c2a789f5c9ae54ea9
lxhfight <lxhfight51@outlook.com>
2018-08-13 20:50:49 +0800

    添加标签集合组件 hx-tags

---
+++ src/hxui/cpts/HxTags.vue
@@ -0,0 +1,35 @@
+<template>
+   <div class="hx-tags">
+     <span :class="['hx-tag', color, size]"
+           v-for="(item, index) in value"
+           v-bind:key="index"
+           v-text="item"></span>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'HxTags',
+  data () {
+    return {}
+  },
+  props: {
+    value: {
+      type: Array,
+      required: true
+    },
+    editable: {
+      type: Boolean,
+      default: false
+    },
+    color: {
+      type: String,
+      default: ''
+    },
+    size: { // 一共包含 sm: 20px   md：27px 和 bg： 34px
+      type: String,
+      default: 'md'
+    }
+  }
+}
+</script>
