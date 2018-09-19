eea042d1a0e84e38db0f2be9669041ae23f0a093
lxhfight <lxhfight51@outlook.com>
2018-09-19 12:56:37 +0800

    对HxSection进行优化，增加展开收缩功能

--- src/hxui/cpts/HxSection.vue
+++ src/hxui/cpts/HxSection.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="hx-section">
-    <header>
+  <div ref="section"
+    :class="['hx-section', expand && 'fold']"
+    @click="doToggleFold">
+    <header class="foldable">
       <span class="title" v-text="title"></span>
       <slot name="right" class="right"></slot>
     </header>
@@ -9,10 +11,27 @@
 </template>
 
 <script>
+import iconCaretDown from './../img/icon/icon-caret-down.png'
 export default {
   name: 'hx-section',
+  data () {
+    return {
+      expand: false,
+      iconCaretDown
+    }
+  },
   props: {
     title: String
+  },
+  methods: {
+    doToggleFold () {
+      this.expand = !this.expand
+    }
+  },
+  mounted () {
+    const dom = this.$refs.section
+    console.log(dom, dom.offsetHeight, dom.style)
+    dom.style.height = dom.offsetHeight + 'px'
   }
 }
 </script>
