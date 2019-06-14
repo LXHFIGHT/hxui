467d0b007754a68225350628fc5711ac8bdcf3ce
lxhfight <lxhfight1@gmail.com>
2019-06-14 16:59:12 +0800

    修改路由中的index文件的引入

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Index from './views/index.vue'
 
 Vue.use(Router)
 
@@ -10,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'Root',
-      component: () => import('./views/index.vue'),
+      component: Index,
       meta: { keepAlive: true },
       children: [
         {
