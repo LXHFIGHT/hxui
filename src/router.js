902444717b75a2ddc3c23252a1de02b0a5a3d427
lxhfight <lxhfight1@gmail.com>
2019-09-17 23:53:00 +0800

    尝试解决屡次在LINUX环境build失败的问题

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'Root',
-      // component: resolve => require(['@/views/index.vue'], resolve),
+      component: resolve => require(['@/views'], resolve),
       meta: { keepAlive: true },
       children: [
         {
