f6f05690cf808ab517ae1e0bd0bfd15d3322315a
lxhfight <lxhfight1@gmail.com>
2019-09-18 00:25:17 +0800

    解决Linux打包不了问题同时增加打包后提示打包接口配置

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,11 @@ function resolve(dir) {
 module.exports = {
   publicPath: '/',
   productionSourceMap: false,
+  chainWebpack: config => {
+    config // 如果不需要打包接口汇报，则注释调本行配置
+      .plugin('webpack-bundle-analyzer')
+      .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+  },
 	configureWebpack: config => {
     config.resolve = {
 			extensions: ['.js', '.vue', '.json'],
