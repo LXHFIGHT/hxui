34f26f0e4af00812dfb4ac9d770b43b8a7f790e0
lxhfight <lxhfight1@gmail.com>
2019-09-27 23:30:57 +0800

    解决引入svg的结构语法的问题

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,22 @@ module.exports = {
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
     svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
       .use('vue-svg-loader')
       .loader('vue-svg-loader')
+      .options({
+        // optional [svgo](https://github.com/svg/svgo) options
+        svgo: {
+          plugins: [
+            { removeDoctype: true },
+            { removeComments: true },
+            { removeViewBox: false }
+          ],
+          removeViewBox: false
+        }
+      })
     config.module
       .rule('js')
       .include
