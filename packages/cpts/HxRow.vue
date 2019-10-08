8dc93495265f66e11fb0e1d082b1c57c436366d0
lxhfight <lxhfight1@gmail.com>
2019-10-08 11:30:31 +0800

    对行组件HxRow的样式排版进行一轮精简和优化

--- packages/cpts/HxRow.vue
+++ packages/cpts/HxRow.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="hx-row" :class="[oneline && 'oneline', !label && 'no-label', labelSize && `${labelSize}-label`]">
+  <div class="hx-row" :class="[
+    oneline && 'oneline',
+    !label && 'null-label',  // 当没有写入label时，行内容 content 显示一行
+    labelSize && `${labelSize}-label`]">
     <label v-if="label">
       {{ label }}
       <span v-if="required" class="tip">*</span>
@@ -8,7 +11,8 @@
     </label>
     <div class="content">
       <slot ></slot>
-      <span class="degree">
+      <span class="right">
+        <slot name="right"></slot>
         <slot name="degree"></slot> 
       </span>
     </div>
@@ -19,6 +23,12 @@
 </template>
 
 <script>
+/**
+ * HxRow 行组件一共有3个插槽：
+ *  label插槽： 置于label中， 一般用于放置字数较少提示信息，如果上传图片规格
+ *  right插槽： 在行内容区域右侧， 一般用于放置单位信息
+ *  tips插槽： 在行内容区域下方，一般用于放置字数多的提示信息，区域更广
+ */
 export default {
   name: 'hx-row',
   props: {
@@ -28,7 +38,7 @@ export default {
     labelSize: { // 行元素标签的宽度， null为不展示标签， 除此之外还有md, bg两种规格
       type: String,
       validator (val) {
-        return ['md', 'bg', 'null'].includes(val)
+        return ['md', 'bg'].includes(val)
       }
     },
     tips: {
@@ -40,7 +50,7 @@ export default {
     },
     required: {
       type: [String, Boolean, Number],
-      default: false
+      default: ''
     }
   },
   data () {
