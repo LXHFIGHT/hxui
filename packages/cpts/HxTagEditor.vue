63c6addfd7acf0b3080521f2b778223a9ce07d7a
lxhfight <lxhfight1@gmail.com>
2019-10-24 23:05:29 +0800

    HxTagEditor: 规范属性命名，增加只读readonly字段，以及不可重复配置noRepeat，支持@change事件

--- packages/cpts/HxTagEditor.vue
+++ packages/cpts/HxTagEditor.vue
@@ -1,78 +1,119 @@
 <template>
   <div class="hx-tag-editor">
     <div class="zone-tags">
-      <div v-for="(item, idx) in value" class="tag" :key="idx">
+      <div v-for="(item, idx) in target" :class="['tag', disabled && 'disabled']" :key="idx">
         {{ item }}
-        <button class="btn-remove" @click="doRemoveTag(idx)">×</button>
+        <button class="btn-remove" v-if="!disabled && !readonly" @click="doRemoveTag(idx)">×</button>
       </div>
       <input 
         :placeholder="placeholder" 
         v-model="content"
         type="text" 
         class="tag-inputer"
-        v-if="!disabled"
+        v-if="!disabled && !readonly"
         @keydown="doCreateTag" />
     </div>
   </div>
 </template>
 <script>
+import toast from './../plugins/toast'
 // 标签编辑器
 export default {
   data () {
     return {
-      content: ''
+      isString: false, // 传入值 value 是否为字符串
+      content: '',
+      target: ''
     }
   },
   props: {
     value: {
-      type: Array,
+      type: [Array, String],
       required: true
     },
-    color: {
+    splitChar: { // 分割不同标签的字符
       type: String,
-      default: 'pink'
+      default: '|'
     },
     placeholder: {
       type: String,
       default: '输入按回车添加标签'
     },
+    noRepeat: {
+      type: [String, Number, Boolean],
+      default: false
+    },
     disabled: {
       type: [Boolean, String, Number],
       default: false
     },
-    onConfirm: { // 回车创建时
+    readonly: {
+      type: [Boolean, String, Number],
+      default: false
+    },
+    onCreated: { // 回车创建时
       type: Function
     },
-    onRemove: { // 当删除指定标签时
+    onRemoved: { // 当删除指定标签时
       type: Function
     }
   },
   methods: {
-    doEnter () {
-      const value = event.target.value
-      this.$emit('input', value)
+    $_initTarget () {
+      if (Array.isArray(this.value)) {
+        this.target = [].concat(this.value)
+        return
+      }
+      this.isString = true
+      if (!this.value) {
+        this.target = []
+      } else {
+        this.target = this.value.split(this.splitChar)
+      }
+    },
+    $_syncTarget () {
+      if (this.isString) {
+        this.$emit('input', this.target.join(this.splitChar))
+      } else {
+        this.$emit('input', this.target)
+      }
+    },
+    $_isAnySameTag () {
+      return this.target.filter(v => v === this.content).length
     },
     doCreateTag () {
       if (!this.content && event.keyCode === 8) { 
         // 内容为空时按删除按钮
-        this.doRemoveTag(this.value.length - 1)
+        this.doRemoveTag(this.target.length - 1)
         return
       }
       if (!this.content || event.keyCode !== 13) {
         return
       }
-      let result = [].concat(this.value)
-      result.push(this.content)
-      this.onConfirm instanceof Function && this.onConfirm(this.content)
-      this.$emit('input', result)
+      if (!this.content.trim()) {
+        this.content = ''
+        return
+      }
+      if (this.noRepeat && this.$_isAnySameTag()) {
+        toast.warn('不能输入重复的标签')
+        return
+      }
+      this.target.push(this.content)
+      this.onCreated instanceof Function && this.onCreated(this.content)
+      this.$emit('change', this.content)
       this.content = ''
+      this.$_syncTarget()
     },
     doRemoveTag (idx) {
-      let result = [].concat(this.value)
-      result.splice(idx, 1)
-      this.onRemove instanceof Function && this.onRemove(this.value[idx])
-      this.$emit('input', result)
+      const tag = this.target[idx]
+      this.target.splice(idx, 1)
+      this.onRemoved instanceof Function && this.onRemoved(tag)
+      this.$emit('change', tag)
+      this.$_syncTarget()
     }
+  },
+  created () {
+    this.$_initTarget()
   }
 }
 </script>
