241c3dcfe387dd37d6d60cf5b73c74f3732abb40
lxhfight <lxhfight1@gmail.com>
2020-05-04 16:04:28 +0800

    优化HxSwitch事件

--- packages/cpts/HxSwitch.vue
+++ packages/cpts/HxSwitch.vue
@@ -2,7 +2,7 @@
   <div @click="doToggleSwitch()"
     :class="['hx-switch',
       disabled && 'disabled',
-      (value ? 'on' : '')]">
+      (on ? 'on' : '')]">
   </div>
 </template>
 
@@ -10,11 +10,19 @@
 export default {
   name: 'HxSwitch',
   data () {
-    return {}
+    return {
+      on: false
+    }
   },
   props: {
+    id: { // 当同一个页面出现多个开关的时候，可以用作区分
+      type: [Number, String]
+    },
     value: {
       type: [Boolean, String, Number]
+    },
+    content: {
+      type: Array
     },
     doToggle: {
       type: Function,
@@ -28,11 +36,33 @@ export default {
     }
   },
   methods: {
+    $_init () {
+      if (Array.isArray(this.content)) {
+        this.on = this.content[0] === this.value
+        return
+      }
+      if (this.value && this.value !== '0') {
+        this.on = true
+      } else {
+        this.on = false
+      }
+    },
     doToggleSwitch () {
       if (this.disabled) {
         return
       }
       const { value } = this
+      const _handle = (val) => {
+        this.$emit('input', val)
+        this.$emit('change', { value: val, id: this.id })
+        this.$forceUpdate()
+        this.doToggle(val, this.id)
+        this.$nextTick(this.$_init)
+      }
+      if (Array.isArray(this.content)) {
+        _handle(this.content[1] === this.value ? this.content[0] : this.content[1])
+        return
+      }  
       let oppositValue = ''
       // 这里对原值进行智能判断，可以判断原值是0或1模式，或者true和false模式 
       if (value === '0' || value === 0) {
@@ -44,10 +74,17 @@ export default {
       } else if (value == null) {
         oppositValue = 1
       }
-      this.$emit('input', oppositValue)
-      this.$emit('change')
-      this.$forceUpdate()
-      this.doToggle(oppositValue)
+      _handle(oppositValue)
+    }
+  },
+  created () {
+    this.$_init()
+  },
+  watch: {
+    value: {
+      handler (newVal) {
+        this.$_init()
+      }
     }
   }
 }
